2f5275b41ea8b23eb3e5bab64fb4644814ae97f8
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-21 13:58:25 -0400

    Add madlibs.py with a madlibs game implementation

---
+++ madlibs/madlibs.py
@@ -0,0 +1,14 @@
+food = input("Please name your favorite food: ")
+name = input("What is your name? ")
+adjective = input("Give an adjective: ")
+animal = input("Give me an animal: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+verb3 = input("Enter a verb: ")
+
+story = "It was" + " " + food + " " + "day at school, and" + " " + name + " " + "was super" + " " + adjective + " " + "for lunch. But when she went outside to eat, a" + " " + animal + " " + "stole her" + " " + food + " " + "!" + " " + name + " " + "chased the" + " " + animal + " " + "all over school. She" + " " + \
+    verb1 + "," + " " + verb2 + ", and" + " " + verb3 + " " + "through the playground. Then she tripped on her" + " " + animal + " " + \
+        "and the" + " " + animal + " " + "escaped! Luckily," + " " + name + \
+            "’s friends were willing to share their" + " " + food + " " + "with her."
+
+print(story)
